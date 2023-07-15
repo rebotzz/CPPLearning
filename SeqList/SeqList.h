2e7bef865e2cf3ecd6a81714fdaf165bded90ab4
rebotzz <rebotzz@163.com>
2023-07-16 05:40:23 +0800

    2023.7.16 顺序表学习

---
+++ SeqList/SeqList.h
@@ -0,0 +1,78 @@
+#pragma once
+
+//头文件
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+
+//宏
+#define SLDataType int	//所存入数据的类型,便于后期修改
+#define PRINTTYPE "%d " //打印类型,便于后期修改
+#define SCANFTYPE "%d"  //输入类型
+
+#if 0
+#define SLCapacity 1000
+#endif
+////静态顺序表
+//// 最大容量不可修改,不方便
+//// 数据类型定义
+//typedef struct SeqList
+//{
+//	int size;
+//	SLDataType arr[SLCapacity];
+//}SeqListInfo;
+
+//类型定义
+//动态顺序表	依据需要开辟空间
+typedef struct SeqList
+{
+	int size;	//当前存储数据个数
+	int SLCapacity;	//容量
+	SLDataType* arr;	//指向所存储类型数据的指针
+}SeqListInfo;
+//菜单选项符号
+enum meun_choice				//1.前插     2.前删    
+{								//3.后插     4.后删    
+	EXIT_SAVE,					//5.中间插   6.中间删  
+	PUSHFRONT,					//7.中间改   0.保存退出
+	POPFRONT,					//8.打印	 9.读取
+	PUSHBACK,
+	POPBACK,
+	PUSHMIDDLE,
+	POPMIDDLE,
+	REVISEMIDDLE,
+	PRINT,
+	GETDATA
+};
+
+//变量声明
+SeqListInfo SLData;
+
+//函数声明 接口函数
+// 初始化
+void SeqListInit(SeqListInfo* ps);
+//后插
+void SeqListPushBack(SeqListInfo* ps,int x);
+//后删
+void SeqListPopBack(SeqListInfo* ps);
+//前插入
+void SeqListPushFront(SeqListInfo* ps, int x);
+//前删
+void SeqlistPopFront(SeqListInfo* ps);
+//中间插入
+void SeqListPushMiddle(SeqListInfo* ps,int pos, int x);
+//中间修改
+void SeqListReviseMiddle(SeqListInfo* ps, int pos, int x);
+//中间删除
+void SeqListPopMiddle(SeqListInfo* ps,int pos);
+//菜单
+void menu();
+//打印数据
+void SeqListPrint(SeqListInfo* ps);
+//读取文件
+void GetData(SeqListInfo* ps);
+//退出保存文件
+void ExitSave(SeqListInfo* ps);
+//销毁空间
+void SeqListDestory(SeqListInfo* ps);
---
+++ SeqList/test.c
@@ -0,0 +1,179 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+//顺序表学习
+//顺序排放,相邻
+
+//目的,实现数据管理
+
+#include "SeqList.h"
+
+void test1()
+{
+	//初始化
+	SeqListInit(&SLData);
+	//后插数据
+	SeqListPushBack(&SLData, 1);
+	SeqListPushBack(&SLData, 2);
+	SeqListPushBack(&SLData, 3);
+	SeqListPushBack(&SLData, 4);
+	SeqListPushBack(&SLData, 5);
+	SeqListPushBack(&SLData, 6);
+	SeqListPushBack(&SLData, 7);
+	SeqListPushBack(&SLData, 8);
+	//打印数据
+	SeqListPrint(&SLData);
+	//后删
+	SeqListPopBack(&SLData);
+	SeqListPopBack(&SLData);
+	SeqListPopBack(&SLData);
+	//打印数据
+	SeqListPrint(&SLData);
+	//后删
+	SeqListPopBack(&SLData);
+	SeqListPopBack(&SLData);
+	SeqListPopBack(&SLData);
+	SeqListPopBack(&SLData);
+	SeqListPopBack(&SLData);
+	SeqListPopBack(&SLData);
+	SeqListPopBack(&SLData);
+	SeqListPopBack(&SLData);
+	SeqListPopBack(&SLData);
+	//后插数据
+	SeqListPushBack(&SLData, 1);
+	SeqListPushBack(&SLData, 2);
+	SeqListPushBack(&SLData, 3);
+	//打印数据
+	SeqListPrint(&SLData);
+
+}
+
+//前插,前删测试
+void test2()
+{
+	//初始化
+	SeqListInit(&SLData);
+	//前插
+	int i = 0;
+	for (i = 0; i < 8; i++)
+	{
+		SeqListPushFront(&SLData, i);
+	}
+	SeqListPrint(&SLData);
+	//前删
+	for (i = 0; i < 10; i++)
+	{
+		SeqlistPopFront(&SLData);
+	}
+	SeqListPrint(&SLData);
+	//前插
+	for (i = 0; i < 4; i++)
+	{
+		SeqListPushFront(&SLData, i + 3);
+	}
+	SeqListPrint(&SLData);
+}
+
+
+//中间插入数据,中间删除测试
+void test3()
+{
+	//初始化
+	SeqListInit(&SLData);
+	int i = 0;
+	for (i = 0; i < 10; i++)
+	{
+		SeqListPushBack(&SLData, i + 1);
+	}
+	SeqListPrint(&SLData);
+	//中间插入
+	SeqListPushMiddle(&SLData, 5, 10);
+	SeqListPushMiddle(&SLData, -1, 11);
+	SeqListPushMiddle(&SLData, 19, 12);
+	SeqListPrint(&SLData);
+
+	//中间修改
+	SeqListReviseMiddle(&SLData,3,0);
+	SeqListPrint(&SLData);
+
+	//中间删除
+	SeqListPopMiddle(&SLData,3);
+	SeqListPopMiddle(&SLData, 3);
+	SeqListPrint(&SLData);
+
+
+}
+
+
+int main()
+{
+	//后插,后删测试
+	//test1();
+
+	//前插,前删测试
+	//test2();
+
+	//中间插入数据,中间删除测试
+	//test3();
+
+
+	int input = 1;
+	SLDataType x = { 0 };
+	int pos = 0;
+	do
+	{
+		//菜单
+		menu();
+		printf("输入操作:>");
+		scanf("%d", &input);
+		switch (input)
+		{
+		case EXIT_SAVE:
+			ExitSave(&SLData);
+			break;
+		case PUSHFRONT:
+			printf("输入插入数据:");
+			scanf(SCANFTYPE, &x);
+			SeqListPushFront(&SLData, x);
+			break;
+		case POPFRONT:
+			SeqlistPopFront(&SLData);
+			break;
+		case PUSHBACK:
+			printf("输入插入数据:");
+			scanf(SCANFTYPE, &x);
+			SeqListPushBack(&SLData, x);
+			break;
+		case POPBACK:
+			SeqListPopBack(&SLData);
+			break;
+		case PUSHMIDDLE:
+			printf("输入插入数据和插入位置:");
+			scanf(SCANFTYPE, &x);
+			scanf("%d", &pos);
+			SeqListPushMiddle(&SLData, pos - 1, x);
+			break;
+		case POPMIDDLE:
+			printf("输入删除位置:");
+			scanf("%d", &pos);
+			SeqListPopMiddle(&SLData, pos - 1);
+			break;
+		case REVISEMIDDLE:
+			printf("输入修改后数据和修改位置:");
+			scanf(SCANFTYPE, &x);
+			scanf("%d", &pos);
+			SeqListReviseMiddle(&SLData, pos - 1, x);
+			break;
+		case PRINT:
+			SeqListPrint(&SLData);
+			break;
+		case GETDATA:
+			GetData(&SLData);
+			break;
+		default:
+			printf("输入错误,重新输入.\n");
+		}
+	} while (input);
+
+
+	return 0;
+}
