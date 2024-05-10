e32ddd6d9a3d96c4d86d6f3f3ab5f6b04601dd42
rebotzz <rebotzz@163.com>
2024-05-11 03:59:06 +0800

    2024.5.11 图：最小生成数算法x2,最短路径算法x2; LRU算法

--- graph/graph/Graph.h
+++ graph/graph/Graph.h
@@ -41,7 +41,7 @@ namespace matrix
 		{
 			for (size_t i = 0; i < n; ++i)
 			{
-				_vertex.push_back(arr[i]);
+				_vertexs.push_back(arr[i]);
 				_indexMap[arr[i]] = i;
 			}
 
@@ -72,8 +72,8 @@ namespace matrix
 
 		void addEdge(const Edge& eg)
 		{
-			assert(eg._dsti >= 0 && eg._dsti < _vertex.size());
-			assert(eg._srci >= 0 && eg._srci < _vertex.size());
+			assert(eg._dsti >= 0 && eg._dsti < _vertexs.size());
+			assert(eg._srci >= 0 && eg._srci < _vertexs.size());
 
 			_matrix[eg._srci][eg._dsti] = eg._weight;
 			// 无向图
@@ -125,14 +125,14 @@ namespace matrix
 
 
 
-		// 广度优先遍历 队列
+		// 广度优先遍历 队列	BFS
 		void BroadPriTraversal(const V& val)
 		{
 			auto it = _indexMap.find(val);
 			if (it == _indexMap.end())
 				throw invalid_argument("没有这个顶点");
 
-			int n = _vertex.size();
+			int n = _vertexs.size();
 			vector<bool> visited(n, false);	// 记录访问过的顶点
 			_BroadPriTraversal(it->second, visited);
 
@@ -151,7 +151,7 @@ namespace matrix
 			queue<int> q;							// 辅助遍历
 			q.push(begin);
 			visited[begin] = true;
-			int n = _vertex.size();
+			int n = _vertexs.size();
 
 			int levelsize = 1;	// 用于区分每层元素，队列同时最多存在两层元素;
 			while (!q.empty())
@@ -160,7 +160,7 @@ namespace matrix
 				{
 					int front = q.front();
 					q.pop();
-					cout << _vertex[front] << " ";
+					cout << _vertexs[front] << " ";
 
 					// 遍历领边矩阵,找到领边
 					for (size_t i = 0; i < n; ++i)
@@ -177,14 +177,14 @@ namespace matrix
 			}
 		}
 
-		// 深度优先遍历	递归
+		// 深度优先遍历	递归	DFS
 		void DeepPriTraversal(const V& val)
 		{
 			auto it = _indexMap.find(val);
 			if (it == _indexMap.end())
 				throw invalid_argument("没有这个顶点");
 
-			int n = _vertex.size();
+			int n = _vertexs.size();
 			vector<bool> visited(n, false);	// 记录访问过的顶点
 			int begin = it->second;
 			visited[begin] = true;
@@ -203,8 +203,8 @@ namespace matrix
 
 		void _DeepPriTraversal(int begin, vector<bool>& visited)
 		{
-			cout << _vertex[begin] << " ";
-			int n = _vertex.size();
+			cout << _vertexs[begin] << " ";
+			int n = _vertexs.size();
 			for (int i = 0; i < n; ++i)
 			{
 				if (_matrix[begin][i] != MAX_W && visited[i] == false)
@@ -219,8 +219,8 @@ namespace matrix
 		// 最小生成树	全局贪心
 		W bruskal(Self& mintree)
 		{
-			size_t n = _vertex.size();
-			mintree._vertex = _vertex;
+			size_t n = _vertexs.size();
+			mintree._vertexs = _vertexs;
 			mintree._indexMap = _indexMap;
 			mintree._matrix.resize(n);
 			for (size_t i = 0; i < n; ++i)
@@ -228,7 +228,7 @@ namespace matrix
 				mintree._matrix[i].resize(n, MAX_W);
 			}
 
-			UnionFindSet<V> ufs(&_vertex[0], _vertex.size());			// 并查集，用于判断是否成环: 多个集合 -> 森林
+			UnionFindSet<V> ufs(&_vertexs[0], _vertexs.size());			// 并查集，用于判断是否成环: 多个集合 -> 森林
 			priority_queue<Edge, vector<Edge>, greater<Edge>> minq;		// 贪心：找到局部最优解，权值最小的边
 
 			// 建立小堆，选出权值最小的边
@@ -251,8 +251,8 @@ namespace matrix
 			{
 				Edge eg = minq.top();					// 取权值最小的边
 				minq.pop();
-				const V& src = _vertex[eg._srci];
-				const V& dst = _vertex[eg._dsti];
+				const V& src = _vertexs[eg._srci];
+				const V& dst = _vertexs[eg._dsti];
 				if (ufs.inSet(src, dst) == false)	// 不成环
 				{
 					ufs.unionSet(src, dst);
@@ -274,77 +274,73 @@ namespace matrix
 		}
 
 
-		// 最小生成树	局部贪心	{x}已经选过的顶点集合 -> {v}未选过的顶点集合 : 避免成环
-		//// 选取{x} -> {v} 权值最小的边
-		//W prim(Self& mintree, const V& src)
-		//{
-		//	// 初始化mintree
-		//	size_t n = _vertex.size();
-		//	mintree._vertex = _vertex;
-		//	mintree._indexMap = _indexMap;
-		//	mintree._matrix.resize(n);
-		//	for (size_t i = 0; i < n; ++i)
-		//	{
-		//		mintree._matrix[i].resize(n, MAX_W);
-		//	}
-
-		//	vector<bool> visited(n, false);
-		//	int srci = getVertexIndex(src);
-		//	queue<int> qe;
-		//	qe.push(srci);
-
-		//	// 建立小堆，选出权值最小的边
-		//	priority_queue<Edge, vector<Edge>, greater<Edge>> minq;		// 贪心：找到局部最优解，权值最小的边
-
-
-		//	int size = n - 1;
-		//	W totalW = W();
-		//	while (!qe.empty())
-		//	{
-		//		int front = qe.front();
-		//		visited[front] = true;
-		//		qe.pop();
-		//		// 把与这个节点相关的边入小堆
-		//		for (size_t i = 0; i < n; ++i)
-		//		{
-		//			if (_matrix[front][i] != MAX_W)
-		//			{
-		//				minq.push(Edge(front, i, _matrix[front][i]));
-		//				if(visited[i] == false) 
-		//					qe.push(i);
-		//			}
-		//		}
-
-		//		Edge eg = minq.top();
-		//		minq.pop();
-		//		// 就像一棵从起点生长的树，最终路过所有顶点
-		//		if (visited[eg._srci] == true && visited[eg._dsti] == false)	// 为了效率使用小堆，代价：判环
-		//		{
-		//			mintree.addEdge(eg);
-		//			visited[eg._dsti] = true;
-		//			size--;
-		//			totalW += eg._weight;
-
-		//			cout << _vertex[eg._srci] << "->" << _vertex[eg._dsti] << " : " << eg._weight << endl;
-		//		}
-		//		//else
-		//		//{
-		//		//	cout << "成环：";
-		//		//	cout << _vertex[eg._srci] << "->" << _vertex[eg._dsti] << " : " << eg._weight << endl;
-		//		//}
-
-		//		if (size == 0) return totalW;
-		//	}
-
-		//	return MAX_W;
-		//}
-
-		// 选取{x} -> {v} 权值最小的边	暴力版本
+		// 最小生成树 局部贪心 {x}已经选过的顶点集合 -> {v}未选过的顶点集合 : 避免成环,选取{x} -> {v} 权值最小的边
+		// 利用小堆优化
 		W prim(Self& mintree, const V& src)
 		{
 			// 初始化mintree
-			size_t n = _vertex.size();
-			mintree._vertex = _vertex;
+			size_t n = _vertexs.size();
+			mintree._vertexs = _vertexs;
+			mintree._indexMap = _indexMap;
+			mintree._matrix.resize(n);
+			for (size_t i = 0; i < n; ++i)
+			{
+				mintree._matrix[i].resize(n, MAX_W);
+			}
+
+			vector<bool> visited(n, false);
+			int srci = getVertexIndex(src);
+			visited[srci] = true;
+			priority_queue<Edge, vector<Edge>, greater<Edge>> minq;		// 建立小堆，选出权值最小的边
+			// 将与srci相邻的边入堆
+			for (size_t i = 0; i < n; ++i)	// 建堆的时间复杂度O(N)
+			{
+				if (_matrix[srci][i] != MAX_W)
+					minq.push(Edge(srci, i, _matrix[srci][i]));
+			}
+
+			int size = n - 1;
+			W totalW = W();
+			// 就像一棵从起点生长的树，最终路过所有顶点
+			while (!minq.empty())
+			{
+				// 选取{x} -> {v} 权值最小的边
+				Edge eg = minq.top();
+				minq.pop();
+				if (visited[eg._dsti] == false)	// 为了效率使用小堆，代价：判环
+				{
+					mintree.addEdge(eg);
+					visited[eg._dsti] = true;
+					size--;
+					totalW += eg._weight;
+
+					// 将与dsti相邻的边入堆
+					for (size_t i = 0; i < n; ++i)
+					{
+						if (_matrix[eg._dsti][i] != MAX_W && visited[i] == false)
+							minq.push(Edge(eg._dsti, i, _matrix[eg._dsti][i]));
+					}
+
+					cout << _vertexs[eg._srci] << "->" << _vertexs[eg._dsti] << " : " << eg._weight << endl;
+				}
+				else
+				{
+					cout << "成环：";
+					cout << _vertexs[eg._srci] << "->" << _vertexs[eg._dsti] << " : " << eg._weight << endl;
+				}
+
+				if (size == 0) return totalW;
+			}
+
+			return MAX_W;
+		}
+
+		// 选取{x} -> {v} 权值最小的边	暴力版本
+		W primViolence(Self& mintree, const V& src)
+		{
+			// 初始化mintree
+			size_t n = _vertexs.size();
+			mintree._vertexs = _vertexs;
 			mintree._indexMap = _indexMap;
 			mintree._matrix.resize(n);
 			for (size_t i = 0; i < n; ++i)
@@ -384,16 +380,317 @@ namespace matrix
 				size--;
 				totalW += eg._weight;
 
-				cout << _vertex[eg._srci] << "->" << _vertex[eg._dsti] << " : " << eg._weight << endl;
+				cout << _vertexs[eg._srci] << "->" << _vertexs[eg._dsti] << " : " << eg._weight << endl;
 				if (size == 0) return totalW;
 			}
 
 			return MAX_W;
 		}
 
+		// 单源最短路径	无负权  类似prim算法	时间复杂度O(n^2) 空间复杂度O(n)
+		// 参数 源点：source 顶点到源点最小权值和数组：distanse 最短路径父顶点数组：parentPath
+		void Dijkstra(const V& src, vector<W>& dist, vector<int>& pPath)
+		{
+			// 初始化
+			size_t n = _vertexs.size();
+			size_t srci = getVertexIndex(src);
+			dist.resize(n, MAX_W);
+			pPath.resize(n, -1);
+			dist[srci] = W();		// srci的权值给一个最小值,自己到自己
+			pPath[srci] = srci;
+
+			vector<bool> visited(n, false);
+			visited[srci] = true;
+			// 可以用小堆，也可以暴力遍历找到最小边
+			priority_queue<Edge, vector<Edge>, greater<Edge>> minq;		// 建立小堆，选出权值最小的边
+			// 将与srci相邻的边入堆
+			for (size_t i = 0; i < n; ++i)	// 建堆的时间复杂度O(N)
+			{
+				if (_matrix[srci][i] != MAX_W)
+					minq.push(Edge(srci, i, _matrix[srci][i]));
+			}
+
+			while (!minq.empty())
+			{
+				// 选取{x} -> {v} 权值最小的边
+				Edge eg = minq.top();
+				minq.pop();
+				// 松弛算法：更新出更短连接路径
+				if (visited[eg._dsti] == false
+					&& dist[eg._srci] + _matrix[eg._srci][eg._dsti] < dist[eg._dsti])	// 判环 + 更新路径
+				{
+					visited[eg._dsti] = true;
+					dist[eg._dsti] = dist[eg._srci] + _matrix[eg._srci][eg._dsti];
+					pPath[eg._dsti] = eg._srci;
+
+					// 将与dsti相邻的边入堆
+					for (size_t i = 0; i < n; ++i)
+					{
+						if (_matrix[eg._dsti][i] != MAX_W && visited[i] == false)
+							minq.push(Edge(eg._dsti, i, _matrix[eg._dsti][i]));
+					}
+
+					cout << _vertexs[eg._srci] << "->" << _vertexs[eg._dsti] << " : " << eg._weight << endl;
+				}
+				else
+				{
+					cout << "成环/不是相对更短的路径：";
+					cout << _vertexs[eg._srci] << "->" << _vertexs[eg._dsti] << " : " << eg._weight << endl;
+				}
+			}
+		}
+
+		// 不用小堆，直接遍历找最小边	2.0		时间复杂度O(n^2) 空间复杂度O(n)
+		void Dijkstra2(const V& src, vector<W>& dist, vector<int>& pPath)
+		{
+			// 初始化
+			size_t n = _vertexs.size();
+			size_t srci = getVertexIndex(src);
+			dist.resize(n, MAX_W);
+			pPath.resize(n, -1);
+			dist[srci] = W();		// srci的权值给一个最小值,便于首次遍历
+			vector<bool> visited(n, false);
+
+			// 每次访问过一个点，最多n次访问完成
+			for (size_t k = 0; k < n; ++k)
+			{
+				// 找到没有访问过的最小权值顶点，作为这次的起点
+				W minW = MAX_W;
+				int cur = -1;
+				for (size_t i = 0; i < n; ++i)
+				{
+					if (visited[i] == false && dist[i] < minW)
+					{
+						cur = i;
+						minW = dist[i];
+					}
+				}
+				visited[cur] = true;
+
+				// 找到与cur相邻的，没有访问过的，所有顶点:  判断，更新路径
+				for (size_t i = 0; i < n; ++i)
+				{
+					if (visited[i] == false && _matrix[cur][i] != MAX_W
+						&& dist[cur] + _matrix[cur][i] < dist[i])
+					{
+						dist[i] = dist[cur] + _matrix[cur][i];	// 更新最小权值
+						pPath[i] = cur;							// 更新父顶点
+					}
+				}
+			}
+		}
+
+		// 单源最短路径	可以有负权  时间复杂度O(n^3) 空间复杂度O(n)
+		// 暴力求解，不像Dijkstra访问过的就不访问，这里同样访问更新
+		bool BellmanFord(const V& src, vector<W>& dist, vector<int>& pPath)
+		{
+			// 初始化
+			size_t n = _vertexs.size();
+			size_t srci = getVertexIndex(src);
+			dist.resize(n, MAX_W);
+			pPath.resize(n, -1);
+			dist[srci] = W();
+			pPath[srci] = srci;
+
+			// 如果某一次顶点K更新路径遇到了负权值，那么途径顶点K的权值都需要改变：再来一次 -> n-1个顶点，最多n-1次
+			// 如果没有遇到中间顶点更新，那么就不用再来一次了
+			for (size_t k = 0; k < n - 1; ++k)
+			{
+				bool midExchage = false;
+				// 以i为起点的所有相邻顶点都更新
+				for (size_t i = 0; i < n; ++i)
+				{
+					for (size_t j = 0; j < n; ++j)
+					{
+						if (_matrix[i][j] != MAX_W
+							&& dist[i] + _matrix[i][j] < dist[j])
+						{
+							dist[j] = dist[i] + _matrix[i][j];
+							pPath[j] = i;
+							midExchage = true;
+
+							cout << _vertexs[i] << " -> " << _vertexs[j] << endl;
+						}
+					}
+				}
+				if (midExchage == false)
+					break;
+			}
+
+			// 判断是否有负权回路：死循环，源点到顶点的权值与绕负权回路圈数有关，可以不断减小
+			//  -> 所以再来一次还能更新
+			for (size_t i = 0; i < n; ++i)
+			{
+				for (size_t j = 0; j < n; ++j)
+				{
+					if (_matrix[i][j] != MAX_W
+						&& dist[i] + _matrix[i][j] < dist[j])
+					{
+						return false;
+					}
+				}
+			}
+			return true;
+		}
+
+		//  多源最短路径 Floyd-Warshall算法	 时间复杂度O(N^3) 空间复杂度O(N^2)
+		void FloydWarshall(vector<vector<W>>& vvdist, vector<vector<int>>& vvpPath)
+		{
+			// 每一行vvdist[i]，vvpPath[i]都是以 i 为源点的最短路径数组
+			// 初始化
+			size_t n = _vertexs.size();
+			vvdist.resize(n);
+			vvpPath.resize(n);
+			for (size_t i = 0; i < n; ++i)
+			{
+				vvdist[i].resize(n, MAX_W);
+				vvpPath[i].resize(n, -1);
+			}
+
+
+			// 初始化权值和路径矩阵：直接相连的边
+			for (size_t i = 0; i < n; ++i)
+			{
+				for (size_t j = 0; j < n; ++j)
+				{
+					if (_matrix[i][j] != MAX_W)
+					{
+						vvdist[i][j] = _matrix[i][j];	// 距离
+						vvpPath[i][j] = i;				// j的上一个顶点
+					}
+
+					if (i == j)
+					{
+						vvdist[i][j] = W();
+					}
+				}
+			}
+			cout << "初始化：" << endl;
+			printMatrix(vvdist, vvpPath);
+
+
+			for (size_t k = 0; k < n; k++)
+			{
+				// k是i和j的中间节点，k可能会更新(从一个点变成多点)
+				for (size_t i = 0; i < n; ++i)
+				{
+					for (size_t j = 0; j < n; ++j)
+					{
+						// 如果i->j 存在中间节点k，i->k->j距离更短，更新
+						// debug: 判断两点间存在路径的不是_matrix[i][k]，而是vvdist[i][k]，
+						// 前者只有直接路径，而后者包含了间接路径
+						if (vvdist[i][k] != MAX_W && vvdist[k][j] != MAX_W
+							&& vvdist[i][k] + vvdist[k][j] < vvdist[i][j])
+						{
+							vvdist[i][j] = vvdist[i][k] + vvdist[k][j];
+							// j的上一个顶点
+							// 如果k和j直接相连，那么就是k
+							// 如果k->j更新了，变为k->...->x->j,那么就是x
+							vvpPath[i][j] = vvpPath[k][j];
+
+							cout << "新增中间节点：" << _vertexs[i] << "->[" << _vertexs[k] << "]->" << _vertexs[j] << endl;
+						}
+					}
+				}
+
+				cout << "k: " << k << endl;
+				printMatrix(vvdist, vvpPath);
+			}
+		}
+
+
+		void printMatrix(vector<vector<W>>& vvdist, vector<vector<int>>& vvpPath)
+		{
+			size_t n = vvdist.size();
+			// 打印矩阵
+			cout << "vvdist:" << endl;
+			printf("     ");
+			for (size_t i = 0; i < n; i++)
+			{
+				printf("%-3d ", i);
+			}
+			cout << endl;
+			printf("     ");
+			for (size_t i = 0; i < n; i++)
+			{
+				printf("%-3c ", _vertexs[i]);
+			}
+			cout << endl;
+
+			for (size_t i = 0; i < n; i++)
+			{
+				printf("%-2d %c ", i, _vertexs[i]);
+				for (size_t j = 0; j < n; j++)
+				{
+					if (vvdist[i][j] != MAX_W)
+						printf("%-3d ", vvdist[i][j]);
+					else
+						printf("%-3c ", '*');
+				}
+				cout << endl;
+			}
+			cout << endl;
+
+			cout << "vvpPath:" << endl;
+			printf("     ");
+			for (size_t i = 0; i < n; i++)
+			{
+				printf("%-3d ", i);
+			}
+			cout << endl;
+			printf("     ");
+			for (size_t i = 0; i < n; i++)
+			{
+				printf("%-3c ", _vertexs[i]);
+			}
+			cout << endl;
+			for (size_t i = 0; i < n; i++)
+			{
+				printf("%-2d %c ", i, _vertexs[i]);
+				for (size_t j = 0; j < n; j++)
+				{
+					if (vvpPath[i][j] != -1)
+						printf("%-3d ", vvpPath[i][j]);
+					else
+						printf("%-3c ", '*');
+				}
+				cout << endl;
+			}
+			cout << "----------------------------" << endl;
+		}
+
+
+		void printShortPath(const V& src, const vector<W>& dist, const vector<int>& pPath)
+		{
+			cout << endl << "最短路径：" << endl;
+
+			int srci = getVertexIndex(src);
+			size_t n = dist.size();
+			for (size_t i = 0; i < n; ++i)
+			{
+				vector<int> path;
+				int parent = i;
+				while (parent != srci)
+				{
+					if (parent == -1) break;
+					path.push_back(parent);
+					parent = pPath[parent];
+				}
+				path.push_back(srci);
+				reverse(path.begin(), path.end());
+
+				for (auto index : path)
+				{
+					cout << _vertexs[index] << " -> ";
+				}
+				cout << " weight: " << dist[i] << endl;
+			}
+
+		}
+
 
 	private:
-		vector<V> _vertex;				// 顶点	_vertex
+		vector<V> _vertexs;				// 顶点	_vertexs
 		map<V, int> _indexMap;			// 存储数值与下标映射关系	_indexmap
 		vector<vector<W>> _matrix;		// 领边矩阵：利用矩阵表示两个顶点对应的边, 权值	_matrix
 	};
@@ -426,10 +723,10 @@ namespace table
 
 		Graph(const V* arr, size_t n)
 		{
-			_vertex.reserve(n);
+			_vertexs.reserve(n);
 			for (size_t i = 0; i < n; ++i)
 			{
-				_vertex.push_back(arr[i]);
+				_vertexs.push_back(arr[i]);
 				_indexMap[arr[i]] = i;
 			}
 
@@ -479,7 +776,7 @@ namespace table
 				cout << i << " : ";
 				while (cur != nullptr)
 				{
-					cout << "[" << _vertex[cur->_srci] << " - " << _vertex[cur->_dsti] << " " << cur->_weight << " "
+					cout << "[" << _vertexs[cur->_srci] << " - " << _vertexs[cur->_dsti] << " " << cur->_weight << " "
 						/*<< "(" << cur->_srci << "->" << cur->_dsti << ")"*/ << "] -->";
 					cur = cur->_next;
 				}
@@ -492,7 +789,7 @@ namespace table
 
 
 	private:
-		vector<V> _vertex;
+		vector<V> _vertexs;
 		map<V, int> _indexMap;								// 存储数值与下标映射关系
 		vector<Edge*> _table;							// 领边表(链表)：表示与这个顶点相连接的边, 权值
 		//vector<pair<Edge, Edge*>> _table;				
--- graph/graph/main.cpp
+++ graph/graph/main.cpp
@@ -34,7 +34,7 @@ void testUnionFindSet()
 	ufs.print();
 	cout << "------------" << endl;
 
-	ufs.unionSet(2, 3);	
+	ufs.unionSet(2, 3);
 	ufs.print();
 	cout << "------------" << endl;
 
@@ -42,11 +42,11 @@ void testUnionFindSet()
 	ufs.print();
 	cout << "------------" << endl;
 
-	ufs.unionSet(6, 6);	
+	ufs.unionSet(6, 6);
 	ufs.print();
 	cout << "------------" << endl;
 
-	ufs.unionSet(7, 1);	
+	ufs.unionSet(7, 1);
 	ufs.print();
 	cout << "------------" << endl;
 
@@ -58,10 +58,10 @@ void testUnionFindSet()
 
 void testGraphMatrix()
 {
-	string arr[] = { "张三", "李四", "王五", "九", "斯科拉", "神", "哈希", "邓布利多"};
+	string arr[] = { "张三", "李四", "王五", "九", "斯科拉", "神", "哈希", "邓布利多" };
 
 	matrix::Graph<string, int> g(arr, sizeof(arr) / sizeof(arr[0]));
-	
+
 	g.print();
 
 	g.addEdge("张三", "李四", 30);
@@ -125,7 +125,7 @@ void testClass()
 	};
 
 	// I think：this指针作为形参传递，而参数压栈传递，所以this指针在栈上，实际传递过程中需要用到寄存器，也在寄存器上
-	A* p = nullptr;		
+	A* p = nullptr;
 	p->print();
 	//p->printA();
 }
@@ -162,6 +162,176 @@ void testGraphMintree()
 
 }
 
+
+void testDijkstra()
+{
+	const char str[] = "syztx";
+	matrix::Graph<char, int, INT_MAX, true> g(str, strlen(str));
+	g.addEdge('s', 't', 10);
+	g.addEdge('s', 'y', 5);
+	g.addEdge('y', 't', 3);
+	g.addEdge('y', 'x', 9);
+	g.addEdge('y', 'z', 2);
+	g.addEdge('z', 's', 7);
+	g.addEdge('z', 'x', 6);
+	g.addEdge('t', 'y', 2);
+	g.addEdge('t', 'x', 1);
+	g.addEdge('x', 'z', 4);
+
+	vector<int> dist;
+	vector<int> parentPath;
+	//g.Dijkstra('s', dist, parentPath);
+	g.Dijkstra2('s', dist, parentPath);
+	g.printShortPath('s', dist, parentPath);
+
+	cout << "------------" << endl;
+
+	// 图中带有负权路径时，贪心策略则失效了。
+	// 测试结果可以看到s->t->y之间的最短路径没更新出来
+	const char str2[] = "sytx";
+	matrix::Graph<char, int, INT_MAX, true> g2(str2, strlen(str2));
+	g2.addEdge('s', 't', 10);
+	g2.addEdge('s', 'y', 5);
+	g2.addEdge('t', 'y', -7);
+	g2.addEdge('y', 'x', 3);
+	vector<int> dist2;
+	vector<int> parentPath2;
+	g2.Dijkstra2('s', dist2, parentPath2);
+	g2.printShortPath('s', dist2, parentPath2);
+
+}
+
+void testBellmanFord()
+{
+	const char str2[] = "sytx";
+	matrix::Graph<char, int, INT_MAX, true> g2(str2, strlen(str2));
+	g2.addEdge('s', 't', 10);
+	g2.addEdge('s', 'y', 5);
+	g2.addEdge('t', 'y', -7);
+	g2.addEdge('y', 'x', 3);
+	vector<int> dist2;
+	vector<int> parentPath2;
+	g2.BellmanFord('s', dist2, parentPath2);
+	g2.printShortPath('s', dist2, parentPath2);
+	cout << "------------" << endl;
+
+
+	const char str[] = "syztx";
+	matrix::Graph<char, int, INT_MAX, true> g(str, strlen(str));
+	g.addEdge('s', 't', 6);
+	g.addEdge('s', 'y', 7);
+	g.addEdge('y', 'z', 9);
+	g.addEdge('y', 'x', -3);
+	g.addEdge('z', 's', 2);
+	g.addEdge('z', 'x', 7);
+	g.addEdge('t', 'x', 5);
+	g.addEdge('t', 'y', 8);
+	g.addEdge('t', 'z', -4);
+	g.addEdge('x', 't', -2);
+	vector<int> dist;
+	vector<int> parentPath;
+	if (g.BellmanFord('s', dist, parentPath))
+		g.printShortPath('s', dist, parentPath);
+	else
+		cout << "负权环路" << endl;
+	cout << "------------" << endl;
+
+
+	// 微调图结构，带有负权回路的测试
+	const char str3[] = "syztx";
+	matrix::Graph<char, int, INT_MAX, true> g3(str3, strlen(str3));
+	g3.addEdge('s', 't', 6);
+	g3.addEdge('s', 'y', 7);
+	g3.addEdge('y', 'z', 9);
+	g3.addEdge('y', 'x', -3);
+	g3.addEdge('y', 's', 1); // 新增
+	g3.addEdge('z', 's', 2);
+	g3.addEdge('z', 'x', 7);
+	g3.addEdge('t', 'x', 5);
+	g3.addEdge('t', 'y', -8); // 更改
+	g3.addEdge('t', 'z', -4);
+	g3.addEdge('x', 't', -2);
+	vector<int> dist3;
+	vector<int> parentPath3;
+	if (g3.BellmanFord('s', dist3, parentPath3))
+	{
+		g3.printShortPath('s', dist3, parentPath3);
+	}
+	else
+	{
+		cout << "存在负权回路" << endl;
+	}
+
+}
+
+void testFloydWarshall()
+{
+	//const char str[] = "syztx";
+	//matrix::Graph<char, int, INT_MAX, true> g(str, strlen(str));
+	//g.addEdge('s', 't', 6);
+	//g.addEdge('s', 'y', 7);
+	//g.addEdge('y', 'z', 9);
+	//g.addEdge('y', 'x', -3);
+	//g.addEdge('z', 's', 2);
+	//g.addEdge('z', 'x', 7);
+	//g.addEdge('t', 'x', 5);
+	//g.addEdge('t', 'y', 8);
+	//g.addEdge('t', 'z', -4);
+	//g.addEdge('x', 't', -2);
+	//vector<vector<int>> vvdist;
+	//vector<vector<int>> vvparentPath;
+	//g.FloydWarshall(vvdist, vvparentPath);
+	//cout << "------------" << endl;
+
+
+	const char str2[] = "sytx";
+	matrix::Graph<char, int, INT_MAX, true> g2(str2, strlen(str2));
+	g2.addEdge('s', 't', 10);
+	g2.addEdge('s', 'y', 5);
+	g2.addEdge('t', 'y', -7);
+	g2.addEdge('y', 'x', 3);
+
+	// 新增：	bug: y->t 路径没有更新出来
+	g2.addEdge('t', 'x', 9);
+	g2.addEdge('x', 's', 2);
+
+	vector<vector<int>> dist2;
+	vector<vector<int>> parentPath2;
+	g2.FloydWarshall(dist2, parentPath2);
+	// 打印任意两点之间的最短路径
+	for (size_t i = 0; i < strlen(str2); ++i)
+	{
+		cout << "源点：" << str2[i] << endl;
+		g2.printShortPath(str2[i], dist2[i], parentPath2[i]);
+		cout << "---------------" << endl << endl;
+	}
+
+
+
+	//const char str3[] = "12345";
+	//matrix::Graph<char, int, INT_MAX, true> g3(str3, strlen(str3));
+	//g3.addEdge('1', '2', 3);
+	//g3.addEdge('1', '3', 8);
+	//g3.addEdge('1', '5', -4);
+	//g3.addEdge('2', '4', 1);
+	//g3.addEdge('2', '5', 7);
+	//g3.addEdge('3', '2', 4);
+	//g3.addEdge('4', '1', 2);
+	//g3.addEdge('4', '3', -5);
+	//g3.addEdge('5', '4', 6);
+	//vector<vector<int>> vvDist3;
+	//vector<vector<int>> vvParentPath3;
+	//g3.FloydWarshall(vvDist3, vvParentPath3);
+	//// 打印任意两点之间的最短路径
+	//for (size_t i = 0; i < strlen(str3); ++i)
+	//{
+	//	cout << "源点：" << str3[i] << endl;
+	//	g3.printShortPath(str3[i], vvDist3[i], vvParentPath3[i]);
+	//	cout << "---------------" << endl << endl;
+	//}
+
+}
+
 int main()
 {
 	try
@@ -170,7 +340,12 @@ int main()
 		//testGraphMatrix();
 		//testGraphTable();
 		//testClass();
-		testGraphMintree();
+		//testGraphMintree();
+
+		//testDijkstra();
+		//testBellmanFord();
+		testFloydWarshall();
+
 	}
 	catch (const exception& e)
 	{
