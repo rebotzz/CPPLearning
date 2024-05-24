bd4bfb8b3c01a9034e04af6f2b4bf34abea48dfe
rebotzz <rebotzz@163.com>
2024-05-24 21:49:09 +0800

    2024.5.24 算法学习:递归

--- graph/graph/test.cpp
+++ graph/graph/test.cpp
@@ -32,53 +32,69 @@ using namespace std;
 //}
 
 
+//int main()
+//{
+//    class Solution {
+//    public:
+//        vector<int> inventoryManagement(vector<int>& stock, int cnt) {
+//            // topK
+//            int n = stock.size();
+//            for (int i = n - 1; i >= 0; --i)
+//                adjustDown(stock, i, n);
+//
+//            vector<int> ret(cnt);
+//            for (int i = 0; i < cnt; ++i)
+//            {
+//                for (int j = 0; j < n - i; ++j)
+//                    cout << stock[j] << " ";
+//                cout << endl;
+//                cout << "swap: " << stock[0] << "  " << stock[n - 1 - i] << endl;
+//
+//                ret[i] = stock[0];
+//                int end = n - 1 - i;
+//                swap(stock[0], stock[end]);
+//                adjustDown(stock, 0, end);
+//            }
+//            return ret;
+//        }
+//
+//        // 向下调整
+//        void adjustDown(vector<int>& arr, int root, int end)
+//        {
+//            int n = end, parent = root, child = 2 * root + 1;
+//            while (child < n)
+//            {
+//                if (child + 1 < n && arr[child + 1] < arr[child]) child++;
+//                if (child < n && arr[child] < arr[parent]) swap(arr[child], arr[parent]);
+//                else break;
+//                parent = child;
+//                child = 2 * parent + 1;
+//            }
+//        }
+//    };
+//
+//    vector<int> arr = { 0,0,1,2,4,2,2,3,1,4 };
+//    auto v = Solution().inventoryManagement(arr, 8);
+//    cout << "outcome: " << endl;
+//    for (auto e : v)
+//        cout << e << " ";
+//    cout << endl;
+//
+//	return 0;
+//}
+
 int main()
 {
-    class Solution {
-    public:
-        vector<int> inventoryManagement(vector<int>& stock, int cnt) {
-            // topK
-            int n = stock.size();
-            for (int i = n - 1; i >= 0; --i)
-                adjustDown(stock, i, n);
+	//const char *pchar = "123456aavb";
+	//char str[] = "123456aavb";
+	//cout << sizeof(pchar) << endl;	// 指针大小
+	//cout << sizeof(str) << endl;	// 数组大小
 
-            vector<int> ret(cnt);
-            for (int i = 0; i < cnt; ++i)
-            {
-                for (int j = 0; j < n - i; ++j)
-                    cout << stock[j] << " ";
-                cout << endl;
-                cout << "swap: " << stock[0] << "  " << stock[n - 1 - i] << endl;
-
-                ret[i] = stock[0];
-                int end = n - 1 - i;
-                swap(stock[0], stock[end]);
-                adjustDown(stock, 0, end);
-            }
-            return ret;
-        }
-
-        // 向下调整
-        void adjustDown(vector<int>& arr, int root, int end)
-        {
-            int n = end, parent = root, child = 2 * root + 1;
-            while (child < n)
-            {
-                if (child + 1 < n && arr[child + 1] < arr[child]) child++;
-                if (child < n && arr[child] < arr[parent]) swap(arr[child], arr[parent]);
-                else break;
-                parent = child;
-                child = 2 * parent + 1;
-            }
-        }
-    };
-
-    vector<int> arr = { 0,0,1,2,4,2,2,3,1,4 };
-    auto v = Solution().inventoryManagement(arr, 8);
-    cout << "outcome: " << endl;
-    for (auto e : v)
-        cout << e << " ";
-    cout << endl;
+	vector<int> arr = { 1,2,3,4,5 };
+	//reverse(arr.begin(), arr.begin() + 3);
+	reverse(arr.begin(), arr.begin() + 2);
+	for (auto e : arr) cout << e << " ";
+	cout << endl;
 
 	return 0;
 }
