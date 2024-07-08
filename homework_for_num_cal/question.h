a56054920ff0098c075018af73ce5d817d2b2150
rebotzz <rebotzz@163.com>
2024-07-08 14:45:38 +0800

    merge history

--- homework_for_num_cal/question.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "my_algrithm_numerical_calculation.h"
-using namespace NumericalCalculation;
-
-void question1_1();
-void question2_1();
-void question3_1();
-void question4_1();
-void question5_1();
--- priority_queue/test.cpp
+++
@@ -1,9 +0,0 @@
-#define _CRT_SECURE_NO_WARNINGS
-#include "priority_queue.h"
-
-
-int main()
-{
-	kozen::test_priority_queue3();
-	return 0;
-}
